2f82024ac5d329a726c4c6abb7b87f76d712ca01
Adam Pasternack <adam.pasternack@gmail.com>
2016-12-10 14:49:45 -0500

    registering repo for dependencey injection

--- TodoApi/src/TodoApi/Startup.cs
+++ TodoApi/src/TodoApi/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using TodoApi.Models;
+
 
 namespace TodoApi
 {
@@ -38,6 +40,9 @@ namespace TodoApi
             services.AddApplicationInsightsTelemetry(Configuration);
 
             services.AddMvc();
+
+            services.AddSingleton<ITodoRepository, TodoRepository>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
